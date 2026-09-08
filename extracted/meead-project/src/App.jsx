d6d7e93c053f67090440ac23351247411bc4ed88
Meead-gallery <44315169+meead-gallery@users.noreply.github.com>
2026-09-08 21:39:04 +0330

    Update App.jsx

--- extracted/meead-project/src/App.jsx
+++ extracted/meead-project/src/App.jsx
@@ -3,7 +3,7 @@ import { createClient } from "@supabase/supabase-js";
 import {
   Lock, Unlock, Copy, Check, CheckCircle2, X, ChevronRight,
   Search, Clock, Upload, Users, Settings as SettingsIcon, Database,
-  AlertTriangle, TrendingUp, TrendingDown, Package, History, LayoutDashboard,Headphones,
+  AlertTriangle, TrendingUp, TrendingDown, Package, History, LayoutDashboard,Headphones,Phone, PhoneCall,
   ShieldAlert ,
 } from "lucide-react";
 
